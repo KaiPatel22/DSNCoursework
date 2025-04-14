3d188a292a937e8900b76b51b21cc4700d0cac28
Kai Patel <106317141+KaiPatel22@users.noreply.github.com>
2025-04-14 14:39:03 +0500

    Started the controller and given it the input params it needs

--- Controller.java
+++ Controller.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Controller {
+    private int cport;
+    private int replicationFactor;
+    private int timeout;
+    private int rebalance_period;
+
+
+
+    public Controller(int cport, int replicationFactor, int timeout, int rebalance_period) {
+        this.cport = cport;
+        this.replicationFactor = replicationFactor;
+        this.timeout = timeout;
+        this.rebalance_period = rebalance_period;
+    }
 }
