93f32c56a7c6933c142051311ff75650497b17f7
Kai Patel <106317141+KaiPatel22@users.noreply.github.com>
2025-04-16 14:47:51 +0500

    Added a dstore and given it the parameters it needs in its constructor

---
+++ DStore1.java
@@ -0,0 +1,13 @@
+public class DStore1 {
+    int port;
+    int cport;
+    int timeout;
+    String file_folder;
+
+    public DStore1(int port, int cport, int timeout, String file_folder) {
+        this.port = port;
+        this.cport = cport;
+        this.timeout = timeout;
+        this.file_folder = file_folder;
+    }
+}
